49cdafd0502eeac6666f451429de81ac32dc02bd
dankdungeon <davidduonggg@gmail.com>
2024-11-30 17:49:42 -0800

    Completed the logic of the project.
    Added the button, hover effect, and color randomization.

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 // script for creating divs
 const container = document.getElementById("container");
 
-for (let i = 0; i < 16; i++) {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("grid");
-  container.appendChild(newDiv);
+function populateContainer(num = 4) {
+  container.innerHTML = ''; 
+  const flexBasis = 100 / num; 
+
+  for (let i = 0; i < num * num; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("grid");
+    newDiv.style.flex = `0 0 ${flexBasis}%`;
+    container.appendChild(newDiv);
+  }
+
+  // hover
+  const grids = document.querySelectorAll(".grid");
+  grids.forEach((element) => {
+    element.addEventListener("mouseenter", () => {
+      var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      element.style.backgroundColor = `#${randomColor}`;
+    });
+  });
 }
+
+// script for button
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+  let num = prompt("How many squares per side for the new grid?");
+  if (num < 100) {
+    populateContainer(num);
+  } else return;
+});
+
+// execution
+populateContainer();
